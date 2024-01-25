1a818600ad7f8432632e86e4883f3e23bd5a3b25
DevanAnderson <devan.odin.anderson@gmail.com>
2024-01-25 13:11:33 -0800

    fixed getValAtLoc(), push_back() and push_front() may need to be fixed as 2 checks fail

--- ulliststr.cpp
+++ ulliststr.cpp
@@ -29,80 +29,156 @@ size_t ULListStr::size() const
 // WRITE YOUR CODE HERE
 
 void ULListStr::push_back(const std::string& val){
-  //if there is nothing in the list yet
-  if(!tail_){
-    //create a new Item
+  //check if there is a ullist
+  if(tail_ == nullptr){
+    //create new node and initialize values
     tail_ = new Item();
-    //set head pointer to point to the new Item
-    head_ = tail_;
-    //set last to the "middle"
-    head_->last = ARRSIZE / 2;
-    //put val in the "middle" - 1
-    head_->val[ARRSIZE / 2 - 1] = val;
-  }
-  //check if tail is full
-  if(tail_->last == ARRSIZE){
-    //create a new Item
-    tail_->next = new Item();
-    //update previous in the new Item
-    tail_->next->prev = tail_;
-    //point tail to the new Item
-    tail_ = tail_->next;
-    //initialize val at 0th index to val passed in
+    tail_->last = 1;
     tail_->val[0] = val;
-    //increment last
-    tail_->last++;
+    head_ = tail_;
   }
-  //if tail is not full
+
+  //check if we need another Item
+  else if(tail_->last == ARRSIZE){
+    //create a new item and intialize values
+    Item* temp = new Item();
+    temp->last = 1;
+    temp->val[0] = val;
+    temp->prev = tail_;
+
+    //update tail and move it
+    tail_->next = temp;
+    tail_ = temp;
+  }
+
   else{
-    //assign val to last index of array and increment last
+    //update val and increment last
     tail_->val[tail_->last++] = val;
   }
+
+  size_++;
 }
 
+  // //if there is nothing in the list yet
+  // if(!tail_){
+  //   //create a new Item
+  //   tail_ = new Item();
+  //   //set head pointer to point to the new Item
+  //   head_ = tail_;
+  //   //move last to 1
+  //   head_->last = 1;
+  //   //insert val at 0
+  //   head_->val[0] = val;
+
+  //   size_++;
+  //   return;
+  // }
+  // //check if tail is full
+  // if(tail_->last == ARRSIZE){
+  //   //create a new Item
+  //   tail_->next = new Item();
+  //   //update previous in the new Item
+  //   tail_->next->prev = tail_;
+  //   //point tail to the new Item
+  //   tail_ = tail_->next;
+  //   //initialize val at 0th index to val passed in
+  //   tail_->val[0] = val;
+  //   //increment last
+  //   tail_->last++;
+  // }
+  // //if tail is not full
+  // else{
+  //   //assign val to last index of array and increment last
+  //   tail_->val[tail_->last++] = val;
+  // }
+
+  // size_++;
+  //}
+
 void ULListStr::push_front(const std::string& val){
-  if(!head_){
-    //create a new Item
-    head_ = new Item();
-    //set head pointer to point to the new Item
-    tail_ = head_;
-    //set last to the "middle"
-    head_->last = ARRSIZE / 2;
-    //put val in the "middle" - 1
-    head_->val[ARRSIZE / 2 - 1] = val;
-  }
-  //check if head is full
-  if(head_->last == ARRSIZE){
-    //create a new Item
-    head_->prev = new Item();
-    //update next in the new Item
-    head_->prev->next = head_;
-    //assign head to the new Item
-    head_ = head_->prev;
-    //update first and last
-    head_->first = ARRSIZE - 1;
-    head_->last = ARRSIZE;
-    //insert val
-    head_->val[head_->first] = val;
-  }
-  //if head is not full
-  else{
-    //assign val to the first index of the array and increment first
-    head_->val[--head_->first] = val;
-  }
+  //check if there is a ullist
+if(head_ == nullptr){
+  head_ = new Item();
+  tail_ = head_;
+  head_->last = ARRSIZE;
+  head_->first = ARRSIZE - 1;
+  head_->val[head_->first] = val;
+}
+//check if we need a new Item
+else if(head_->first == 0){
+  //create a new item and initalize values
+  Item* temp = new Item();
+  temp->first = ARRSIZE - 1;
+  temp->last = ARRSIZE;
+  temp->val[temp->first] = val;
+  temp->next = head_;
+
+  //update and move head pointer
+  head_->prev = temp;
+  head_ = temp;
+}
+
+else{
+
+}
+
+
+
+  // if(!head_){
+  //   //create a new Item
+  //   head_ = new Item();
+  //   //set head pointer to point to the new Item
+  //   tail_ = head_;
+  //   //set first and last to proper value
+  //   head_->first = ARRSIZE - 1;
+  //   head_->last = ARRSIZE;
+  //   //insert val
+  //   head_->val[head_->first] = val;
+
+  //   size_++;
+  //   return;
+  // }
+  // //check if head is full
+  // if(head_->last == ARRSIZE){
+  //   //create a new Item
+  //   head_->prev = new Item();
+  //   //update next in the new Item
+  //   head_->prev->next = head_;
+  //   //assign head to the new Item
+  //   head_ = head_->prev;
+  //   //update first and last
+  //   head_->first = ARRSIZE - 1;
+  //   head_->last = ARRSIZE;
+  //   //insert val
+  //   head_->val[head_->first] = val;
+  // }
+  // //if head is not full
+  // else{
+  //   //assign val to the first index of the array and increment first
+  //   head_->val[--head_->first] = val;
+  // }
+  // size_++;
 }
 
 void ULListStr::pop_back(){
+  //check if there is a linked list
+  if(tail_== nullptr){
+    return;
+  }
+
   //check if there is only one element
-  if(tail_->last == tail_->first - 1){
+  if(tail_->last == 1){
     //check if there is only one node
-    if(head_ == tail_){
+    if(head_ == tail_ && head_ != nullptr){
       //delete node and make head and tail pointers null
       delete head_;
       head_ = nullptr;
       tail_ = nullptr;
+      size_ = 0;
       return;
   }
+
+
     //move tail back one
     tail_ = tail_->prev;
     //delete the old tail
@@ -115,6 +191,8 @@ void ULListStr::pop_back(){
     //decrement last
     tail_->last--;
   }
+
+  size_--;
 }
 
 void ULListStr::pop_front(){
@@ -139,6 +217,7 @@ void ULListStr::pop_front(){
     //increment first
     head_->first++;
   }
+  size_--;
 }
 
 std::string const& ULListStr::back() const{
@@ -150,68 +229,18 @@ std::string const& ULListStr::front() const{
 }
 
 std::string* ULListStr::getValAtLoc(size_t loc) const{
+  size_t location = 0;
   Item* temp = head_;
-  size_t count = 0;
-  while(count != loc){
-    count++;
+  while(temp != nullptr){
+    for(size_t i = temp->first; i < temp->last; i++){
+      if(location == loc){
+        return &(temp->val[i]);
+      }
+      location++;
+    }
+    temp = temp->next;
   }
-
-
-
-  // //checks if ull is empty
-  // if(head_ == nullptr){
-  //   return nullptr;
-  // }
-
-  // Item* temp = head_;
-
-  // size_t count = 0;
-  // size_t pos = head_->first;
-  // while(count != loc){
-  //   pos++;
-  //   count++;
-  //   if(pos == ARRSIZE){
-  //     pos = 0;
-  //     if(temp->next == nullptr){
-  //       return nullptr;
-  //     }
-  //     temp = temp->next;
-  //   }
-  // }
-
-  // return &(temp->val[pos]);
-
-
-
-  // //come back to this if getValAtLoc doesn't work
-  // if(loc > size_){
-  //   return nullptr;
-  // }
-  // Item* temp = head_;
-  // size_t pos = head_->first;
-  // size_t count = 0;
-  // while(count != loc){
-  //   count++;
-  //   pos++;
-  //   if(pos == ARRSIZE){
-  //     if(temp->next == nullptr){
-  //       return nullptr;
-  //     }
-  //     temp = temp->next;
-  //     pos = 0;
-  //   }
-  //   // //if position is at the max size, go to the next node
-  //   // if(pos == ARRSIZE){
-  //   //   temp = temp->next;
-  //   //   pos = 0;
-  //   // }
-  //   // pos++;
-  //   // // //if we are in the tail node and reached the last index, return null
-  //   // // if(pos == temp->last - 1 && temp == tail_){
-  //   // //   return NULL;
-  //   // // }
-  // }
-  // return &(temp->val[pos]);
+  return nullptr;
 }
 
 void ULListStr::print()const{
